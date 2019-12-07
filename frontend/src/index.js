2872c70a9805d85d464827f824fbc03ad831fef6
Felipe <fb.dc.col@gmail.com>
2019-12-07 09:30:49 -0300

    browsify watchify to use npm and .gitignore fore node_modules

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+var Yijing = require('i-ching');
 document.addEventListener("DOMContentLoaded", function() {
     // const app = new App()
     const apiAdapter = new ApiAdapter()
@@ -9,14 +10,32 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log(result[hexnum - 1])
         })
     }
+    
+    getHexagram(4)
+    function cast(question){
+        return Yijing.ask(`${question}`);
+    }
+    
+    
+    var reading = cast("hi")
+    console.log(reading)
+    console.log("test5")
+    
+    //     var reading = iChing.ask('to be or not to be?');
+// console.log('%d %s %s', 
+//             reading.hexagram.number, 
+//             reading.hexagram.character,
+//             reading.hexagram.names.join(', '));
+ 
+// if (reading.change) {
+//   console.log('changing lines: %j', reading.change.changingLines);
+//   console.log('change to hexagram: %d %s %s', 
+//               reading.change.to.number, 
+//               reading.change.to.character, 
+//               reading.change.to.names.join(', '));
+// } else {
+//   console.log('no changing lines');
+// }
 
-    getHexagram(2)
-    // console.log(allHexagrams)
-    // console.log(Array.from(allHexagrams))
-    // test that we can get data from the backend
-        // const BACKEND_URL = 'http://localhost:3000';
-        // fetch(`${BACKEND_URL}/api/v1/hexagrams/1`)
-        // .then(response => response.json())
-        // .then(parsedResponse => console.log(parsedResponse));
     
 });
---
+++ frontend/src/main.js
@@ -0,0 +1,20 @@
+var iChing = require('i-ching');
+// var Yijing = require('i-ching');
+
+// // const cast = (question) => {
+// //     return Yijing.ask(`${question}`);
+// // }
+
+
+// // export {cast};
+// function cast(question){
+//     return Yijing.ask(`${question}`);
+// }
+
+// // var reading = askYijing("hi")
+// // console.log(reading)
+
+
+// module.exports = function cast(question){
+//     return iChing.ask(`${question}`);
+// }
