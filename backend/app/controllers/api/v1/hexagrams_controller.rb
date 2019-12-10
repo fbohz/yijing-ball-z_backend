31165ce5b07102cad31bc89b400aefabcf379d2f
Felipe <fb.dc.col@gmail.com>
2019-12-10 15:41:00 -0300

    quick refactor handling errors hexagram controller

--- backend/app/controllers/api/v1/hexagrams_controller.rb
+++ backend/app/controllers/api/v1/hexagrams_controller.rb
@@ -1,13 +1,22 @@
 class Api::V1::HexagramsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
     def index
         @hexagrams = Hexagram.all
-    
         render json: @hexagrams, status: 200
-      end
+    end
       
       def show
         @hexagram = Hexagram.find(params[:id])
-        
         render json: @hexagram, status: 200
       end
+
+      private
+      
+    def not_found  
+        render json: {
+          status: 500,
+          errors: ['Server error. Record(s) not found']
+        }
+    end 
 end
