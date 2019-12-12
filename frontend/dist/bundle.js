545d04c476f273fff88085561f6ef1148b37f5c9
Felipe <fb.dc.col@gmail.com>
2019-12-12 10:09:09 -0300

    logic of showing change hexagram if provided

--- frontend/dist/bundle.js
+++ frontend/dist/bundle.js
@@ -20814,32 +20814,39 @@ var Yijing = require('i-ching');
 document.addEventListener("DOMContentLoaded", function() {
     const castButton = document.querySelector(".cast-button")
     const hexagram = new Hexagram()
-    const home = document.querySelector("div.container")
     const castHexagram = () => {
         let reading = Yijing.ask()
         num = reading.hexagram.number
         hexagram.getHexagram(num)
+        // console.log(typeof num)
+        const numo = 2
         renderHexagrams(num)
     }
     
-    function renderHexagrams(resultsHex, initialHex) {
+    castButton.addEventListener("click", function(){
+        document.querySelector("div.container").remove()
+         castHexagram()
+         // castButton.classList.add("casted");
+     });
+     
+    function renderHexagrams(resultsHex, changeHex) {
+        // document.querySelector("div.container").remove()
+        // data Attribute in HTML is used to specify the URL of the Embedded file of an Object.
+        document.getElementById('result_hex').setAttribute('data',(`lib/hexagrams.svg#${resultsHex}`))
 
-        // document.getElementById('result_hex').setAttribute('data',(`lib/hexagrams.svg#${resultsHex}}`))
-        document.getElementById('result_hex').setAttribute('data',(`lib/hexagrams.svg#4`))
+        document.getElementById('results_container').style.display = 'inline-grid'
+        // const secondHex = document.getElementById('second_hexagram')
+        
+        if (changeHex) {
+            document.getElementById('change_hex').setAttribute('data',(`lib/hexagrams.svg#${changeHex}`))
+        } else {
+            document.getElementById('second_hexagram').style.display = 'none'
+        }
         let test = document.getElementById('result_hex')
         console.log(test)
-        document.getElementById('results_container').style.display = 'inline-grid'
-
-        // document.getElementById('result_hex').setAttribute('data',('hex.svg#' + right_num))
-        // document.getElementById('change_hex').setAttribute('data',('hex.svg#' + left_num))
     }
 
 
-   castButton.addEventListener("click", function(){
-        castHexagram()
-        home.remove()
-        // castButton.classList.add("casted");
-    });
         
     // if (castButton.classList.contains("casted")) {
     //     // console.log(test)
