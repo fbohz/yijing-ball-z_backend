a4aa9de3aedbd2f9c1c5c0249e5f855c7ae7ed7e
Felipe <fb.dc.col@gmail.com>
2019-12-13 19:15:52 -0300

    revert js server will try doing backend again

--- frontend/server.js
+++ frontend/server.js
@@ -1,15 +1,18 @@
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-const PORT=8080; 
+// const PORT=8080; 
 
-fs.readFile('./index.html', function (err, html) {
+// fs.readFile('./index.html', function (err, html) {
 
-    if (err) throw err;    
+//     if (err) throw err;    
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(PORT);
+// });
+
+// add this to package.json scripts to be able to run 'npm start'
+    // "start": "node.js"
